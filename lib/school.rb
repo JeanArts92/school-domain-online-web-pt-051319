70c9203bf14301e8c7b41cfa95689033555beb9b
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-26 17:47:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,26 @@
 require 'pry' 
 
-class School
-  attr_accessor :name, :roster
-
-  d
+class School 
+  attr_accessor :name 
+  
+  
+  
+  def initialize(name)
+    @name = name 
+    roster
+    
+  end 
+  
+  def roster
+    roster = {}
+  end 
+  
+  def add_student(name, grade)
+    roster[grade] = []
+    roster[grade] << name
+    binding.pry
+  end 
+    
+  
+end 
 
