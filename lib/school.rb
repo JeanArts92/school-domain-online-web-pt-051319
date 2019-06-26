850d6a19bfde1711609f51d26d84fc058fd6164d
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-26 17:20:45 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,2 @@
 # code here!
 
-class School
-  attr_reader :roster, :student
-
-
-def initialize(name)
-  @name = name
-  @roster = {}
-end
-
-def add_student(student, grade)
-  @roster[grade] ||= []
-  @roster[grade] << student
-  
-end
-
-def grade(grade)
-  @roster[grade]
-end
-  
-def sort 
-  @roster.each do |grade, students|
-    @roster[grade] = students.sort
-  end
-end 
-
-end
