4e8c8284d1e86c26c90b0f4edcdec522bfc92384
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-06-26 17:46:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,13 +7,9 @@ class School
   
   def initialize(name)
     @name = name 
-    roster
-    
-  end 
-  
-  def roster
     roster = {}
   end 
+  
   
    def add_student(student_name, grade)
     roster[grade] ||= []
